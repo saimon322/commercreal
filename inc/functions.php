a9779ed0f44fc5028ce7d860e4d9392d8b9e4585
Siemens <saimon.kat23@gmail.com>
2021-09-19 16:00:34 +0300

    Loupe img

--- inc/functions.php
+++ inc/functions.php
@@ -134,3 +134,8 @@ function get_post_primary_category_name()
     
     return null;
 }
+
+function str_prepos($text) {
+    $text = preg_replace('/\x20([а-яА-ЯёЁыЫa-zA-Z]{1,4})\x20/i', ' \\1&nbsp;', $text);
+    return $text;
+}
