99cc36f20d7183c0575123653d9d8e08b85f68b0
Saimon Kat <saimon.kat23@gmail.com>
2022-02-01 08:20:08 +0300

    homepage update

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,15 @@
 ; (function ($) {
     "use strict";
 
+    const appHeight = () => {
+        document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
+    }
+    window.addEventListener('DOMContentLoaded', appHeight)
+    window.addEventListener('resize', appHeight)
+    
+    const mobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    mobileDevice && window.addEventListener('scroll', appHeight)    
+
     var nav_offset_top = $('header').height() + 100;
     /*-------------------------------------------------------------------------------
       Navbar 
